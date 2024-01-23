ab50bc32be2abb005f27c31dd11d9de467b2e117
LarienT <tokopododi@icloud.com>
2024-01-23 18:58:07 +0300

    third commit

--- task4.py
+++ task4.py
@@ -4,12 +4,35 @@
 # Создаете 2 разных объекта, они должны складывать, вычитать.
 # **Так же реализуйте бонусную систему.
 
-from abc import abstractmethod
+class ProductBasket:
+    def __init__(self):
+        self.products = {}  # словарь для хранения продуктов в корзине
+
+    def add_product(self, product, quantity):  # добавление продукта в корзину
+        if product in self.products:
+            self.products[product] += quantity
+        else:
+            self.products[product] = quantity
+
+    def remove_product(self, product, quantity):  # удаление продукта из корзины
+        if product in self.products:
+            if self.products[product] <= quantity:
+                del self.products[product]
+            else:
+                self.products[product] -= quantity
+
+    def show_products(self):  # вывод продуктов в корзине
+        for product, quantity in self.products.items():
+            print(f"{product}: {quantity}")
 
 
-@abstractmethod
-class basket:
-    count = 0
+# User = ProductBasket()
+# while True:
+#     product = input("Enter: ")
+#     if product == "":
+#         break
+#     else:
+#         quantity = int(input(""))
+#         User.add_product(product, quantity)
 
-    def __init__(self, product: dict):
-        self.product = product
+User.show_products()
---
+++ task5.py
@@ -0,0 +1,9 @@
+class Tomato:
+    states = ["", "", ""]
+
+    def __init__(self, state, index):
+        self.__state = state
+        self.__index = index
+
+    def grow(self):
+        pass
